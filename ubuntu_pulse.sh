5dd86034fe1f47b3bab91e071de81fa29b442c96
pstngh <40343616+pstngh@users.noreply.github.com>
2019-02-20 23:48:27 -0500

    Update ubuntu_pulse.sh

--- ubuntu_pulse.sh
+++ ubuntu_pulse.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-sudo apt-get --purge autoremove thunderbird parole pidgin xfburn gnome-mines gnome-sudoku sgt-puzzles ristretto simple-scan blueman bluez*
+sudo apt-get --purge autoremove thunderbird parole pidgin xfburn gnome-mines gnome-sudoku sgt-puzzles ristretto simple-scan blueman bluez* -y
 sudo dpkg --add-architecture i386
-wget -nc https://dl.winehq.org/wine-builds/Release.key
-sudo apt-key add Release.key
-sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
 sudo add-apt-repository ppa:snwh/ppa -y
 sudo add-apt-repository ppa:scribus/ppa -y
 #
 sudo apt update -y
 sudo apt install mpv rhythmbox paper-icon-theme arc-theme gimp gnome-disk-utility scribus -y
-
+sudo apt install --install-recommends winehq-stable
 
 #mouseacc
 sudo tee /usr/share/X11/xorg.conf.d/50-mouse.conf >/dev/null << EOF
