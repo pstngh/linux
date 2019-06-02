d0e099a63b08e0aa25b4c34b5481dec0a1c22225
pstngh <40343616+pstngh@users.noreply.github.com>
2019-06-02 11:31:35 -0400

    Update pulse.sh

--- pulse.sh
+++ pulse.sh
@@ -7,13 +7,15 @@ sudo pacman -Syu --noconfirm --needed mpv rhythmbox
 #yay -S --noconfirm gimp
 #yay -S --noconfirm kodi
 #yay -S --noconfirm skypeforlinux-bin
-yay -Syu --noconfirm teamviewer-beta
+#yay -Syu --noconfirm teamviewer-beta
 #yay -S --noconfirm tiny-media-manager
-#yay -S --noconfirm filebot
 #yay -S --noconfirm youtube-dl-gui-git
-#yay -S --noconfirm paper-icon-theme-git
-#yay -S --noconfirm arc-gtk-theme
-#yay -S --noconfirm wire-desktop
+yay -S --noconfirm paper-icon-theme
+yay -S --noconfirm arc-gtk-theme
+yay -S --noconfirm wire-desktop
+yay -S --noconfirm adapta-gtk-theme
+yay -S --noconfirm anydesk
+#yay -S --noconfirm brasero
 
 #sudo gpasswd -a pulse audio
 
@@ -21,23 +23,18 @@ yay -Syu --noconfirm teamviewer-beta
 
 sudo systemctl enable fstrim.timer
 
-#mouseacc
-sudo tee /etc/X11/xorg.conf.d/50-mouse.conf &>/dev/null << EOF
-Section "InputClass"
-	Identifier "Mouse"
-	Driver "libinput"
-	MatchIsPointer "yes"
-	Option "AccelProfile" "flat"
-EndSection
-EOF
 
-cd
-wget https://download.teamviewer.com/download/version_11x/teamviewer_qs.tar.gz -O teamviewer.tar.xz
-tar xf teamviewer.tar.xz
+sudo rm -rf /usr/share/gnome-shell/extensions
+wget -O steal-my-focus@kagesenshi.org.zip "https://github.com/tak0kada/gnome-shell-extension-stealmyfocus/archive/master.zip"
+#curl -L -O https://github.com/tak0kada/gnome-shell-extension-stealmyfocus/archive/master.zip
+unzip steal-my-focus@kagesenshi.org.zip
+mv gnome-shell-extension-stealmyfocus-master ~/.local/share/gnome-shell/extensions/steal-my-focus@kagesenshi.org
+rm ~/.local/share/gnome-shell/extensions/steal-my-focus@kagesenshi.org/Makefile
+rm ~/.local/share/gnome-shell/extensions/steal-my-focus@kagesenshi.org/README.md
+wget -O gnome-shell-extension-installer "https://github.com/brunelli/gnome-shell-extension-installer/raw/master/gnome-shell-extension-installer"
+sudo chmod +x gnome-shell-extension-installer
+sudo mv gnome-shell-extension-installer /usr/bin/
+gnome-shell-extension-installer 1160 19 118 615 1379 --restart-shell
 
-echo ubuntu | sudo -S systemctl enable teamviewerd.service
-echo ubuntu | sudo -S systemctl start teamviewerd.service
-
-
-
-teamviewer
+sudo pacman -R linux
+sudo grub-mkconfig -o /boot/grub/grub.cfg
