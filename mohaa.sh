f60e73eae875757fe6988fa6b980c70ca5c05616
pstngh <40343616+pstngh@users.noreply.github.com>
2020-04-30 00:58:45 -0400

    Update mohaa.sh

--- mohaa.sh
+++ mohaa.sh
@@ -2,6 +2,10 @@
 
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1S2mpjaYgibyt_64Xgxpbvf8sAWEd6fuj' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1S2mpjaYgibyt_64Xgxpbvf8sAWEd6fuj" -O mohaa.zip && rm -rf /tmp/cookies.txt
 
+sudo dpkg --add-architecture i386
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install libstdc++6:i386
 sudo apt install unzip
 unzip mohaa.zip
 cd mohaa
