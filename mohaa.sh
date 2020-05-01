77dba1ce78affef4ba4d365532cb3c788e34a0db
pstngh <40343616+pstngh@users.noreply.github.com>
2020-05-01 12:45:50 -0400

    Update mohaa.sh

--- mohaa.sh
+++ mohaa.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1S2mpjaYgibyt_64Xgxpbvf8sAWEd6fuj' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1S2mpjaYgibyt_64Xgxpbvf8sAWEd6fuj" -O mohaa.zip && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=121PKLtd5K5sWcJqXqlAJWDK6DIcRT_NG' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=121PKLtd5K5sWcJqXqlAJWDK6DIcRT_NG" -O mohaa.zip && rm -rf /tmp/cookies.txt
 
 sudo dpkg --add-architecture i386
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install libstdc++6:i386
+#sudo apt-get install libstdc++6:i386
 sudo apt install unzip
 unzip mohaa.zip
 cd mohaa
