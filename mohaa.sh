7483c5e6bb7bc1f1182b34667e312c1b89007576
pstngh <40343616+pstngh@users.noreply.github.com>
2020-06-11 23:41:08 -0400

    Update mohaa.sh

--- mohaa.sh
+++ mohaa.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1LRrh5ZYHPMz1KF3KNPv_Eu3nqkR1hTel' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1LRrh5ZYHPMz1KF3KNPv_Eu3nqkR1hTel" -O mohaa.zip && rm -rf /tmp/cookies.txt
-
-#chmod 775 ./mohaa_lnxed ./gs.sh ./gsload.sh
 sudo dpkg --add-architecture i386
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install libstdc++5:i386 -y
 sudo apt install unzip -y
 sudo apt install fail2ban -y
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1LRrh5ZYHPMz1KF3KNPv_Eu3nqkR1hTel' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1LRrh5ZYHPMz1KF3KNPv_Eu3nqkR1hTel" -O mohaa.zip && rm -rf /tmp/cookies.txt
 unzip mohaa.zip
 cd mohaa
-chmod 777 mohaa_lnxded
+chmod 775 ./mohaa_lnxded ./gs.sh ./gsload.sh
